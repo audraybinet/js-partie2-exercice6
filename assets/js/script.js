3778a55c49f9c7a4b9bcd224b35dc6453dc68b66
audraybinet <audreybinet@yahoo.fr>
2019-10-29 09:48:35 +0100

    exo6

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//comme sur l'exercice 5
+function changeImage(lien, id) {
+document.getElementById(id).src=lien;
+}
+//creation autre fonction dans html/ mais même fonction en gros.
+function reverseImg(lien, id) {
+document.getElementById(id).src=lien;
+}
